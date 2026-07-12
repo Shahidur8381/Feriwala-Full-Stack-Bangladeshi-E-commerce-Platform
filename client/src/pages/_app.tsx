8d916a53d21981ab50c68d8559855d7886e60d55
Shahidur8381 <shahidur8381@gmail.com>
2026-07-12 17:49:32 +0600

    style: premium redesign for seller dashboard and fix page titles

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import { AuthProvider } from '../contexts/AuthContext';
 import { CartProvider } from '../contexts/CartContext';
 import Header from '../components/Header';
@@ -8,11 +9,17 @@ import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <Component {...pageProps} />
-      </CartProvider>
-    </AuthProvider>
+    <>
+      <Head>
+        <title>FeriWala - Bangladeshi E-commerce</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <AuthProvider>
+        <CartProvider>
+          <Component {...pageProps} />
+        </CartProvider>
+      </AuthProvider>
+    </>
   );
 }
 
